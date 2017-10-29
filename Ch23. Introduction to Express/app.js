df061557427091b82183c24fbd5b9ecff8bc26f4
Taaniel Magi <28940651+YellowBackCorn@users.noreply.github.com>
2017-10-29 16:30:39 +0300

    4th Commit

--- Ch23. Introduction to Express/app.js
+++ Ch23. Introduction to Express/app.js
@@ -6,4 +6,8 @@ app.get('/', function (req, res) {
     res.send('this is the homepage');
 });
 
+app.get('/contact', function (req, res) {
+    res.send('this is the contact page');
+});
+
 app.listen(3000);
