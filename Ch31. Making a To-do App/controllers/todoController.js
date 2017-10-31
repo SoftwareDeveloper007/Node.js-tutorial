3135303b7c454e8bed73ea93889da9c561df52a9
Taaniel Magi <28940651+YellowBackCorn@users.noreply.github.com>
2017-10-31 03:00:10 +0300

    19th Commit

--- Ch31. Making a To-do App/controllers/todoController.js
+++ Ch31. Making a To-do App/controllers/todoController.js
@@ -15,26 +15,34 @@ var itemOne = Todo({item: 'buy flowers'}).save(function (err) {
     console.log('item saved');
 });
 
-var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+//var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function (app) {
 
     app.get('/todo', function (req, res) {
         console.log("GET command");
-        res.render('todo', {todos: data});
+        // get data from mongodb and pass it to view
+        Todo.find({}, function (err, data) {
+            if(err) throw err;
+            res.render('todo', {todos: data});
+        })
     });
 
     app.post('/todo', urlencodedParser, function (req, res) {
         console.log("POST command");
-        data.push(req.body);
-        res.json(data);
+        // get data from the view and add it to mongodb
+        var newTodo = Todo(req.body).save(function (err, data) {
+            if(err) throw err;
+            res.json(data);
+        });
     });
 
     app.delete('/todo/:item', function (req, res) {
-        data = data.filter(function (todo) {
-            return todo.item.replace(/ /g, '-') !== req.params.item;
-        });
-        res.json(data);
+        // delete the requested item from mongodb
+        Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function (err, data) {
+            if(err) throw err;
+            res.json(data);
+        })
     });
 };
